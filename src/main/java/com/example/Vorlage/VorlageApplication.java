216c81d7841e8ec026b556738f85cd0043ad3eb3
Mattis Henzel <111503864+ShadowCucumber@users.noreply.github.com>
2023-04-13 12:47:48 +0200

    Initial commit

---
+++ src/main/java/com/example/Vorlage/VorlageApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vorlage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorlageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorlageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Vorlage/VorlageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vorlage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorlageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
